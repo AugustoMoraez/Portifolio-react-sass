a679d1d6f18f7b8bc609e4a2a5d942676be43b63
Augusto Moraes <supergamerbros2015@gmail.com>
2023-04-18 20:20:15 -0300

    fish

--- src/comp/ProjetsContainer.jsx
+++ src/comp/ProjetsContainer.jsx
@@ -4,6 +4,8 @@ export const ProjetsContainer = () => {
     return(
         <section >
             <h2>Projetos</h2>
+            <p>Essa parte do portifolio ainda nao esta concluida, mas voce pode ver meus projetos nos destaques do meu perfil do linkedin</p>
+            <a href="https://www.linkedin.com/in/moraesaugusto/" className="btn">Linkedin</a>
         </section>
     )
 }
